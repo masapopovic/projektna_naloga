87e5e9b453b56657d0e32fbdeaefbdd19303f5d5
masapopovic <mp13284@student.uni-lj.si>
2024-08-16 19:29:35 +0200

    sprememba na obravnavo 2600 iger

--- funkcije/shranjevanje_spletnih_strani.py
+++ funkcije/shranjevanje_spletnih_strani.py
@@ -8,7 +8,7 @@ from orodja import pripravi_imenik, shrani_spletno_stran
 
 #zajela bom prvih 20 strani tistih iger, ki imajo ESRB = MATURE
 
-st_strani = 20
+st_strani = 26
 for stran in range(1, st_strani + 1):
     url = f"https://gamesdb.launchbox-app.com/games/results/%7C{stran}?esrb=mature"
     odziv = requests.get(url)
@@ -29,12 +29,12 @@ def odpri_spletne_strani(st_strani):
 
 import time
 def oblikuj_spletne_strani(st_strani):
-    stevec = 1
+    stevec = 2001
     for del_url in odpri_spletne_strani(st_strani):
         url = f'https://gamesdb.launchbox-app.com{del_url}'
         shrani_kot = f'spletne_strani/igra_{stevec}.html'
         shrani_spletno_stran(url, shrani_kot)
         stevec += 1
-        time.sleep(1)
+        #time.sleep(1)
 
-oblikuj_spletne_strani(20)
+oblikuj_spletne_strani(26)
