3688bb554aed823f1721c5b5669ec0051f9adb6c
masapopovic <mp13284@student.uni-lj.si>
2024-08-18 12:50:01 +0200

    popravek kode pri zaloznikih in razvijalcih

--- funkcije/iskanje_vzorcev.py
+++ funkcije/iskanje_vzorcev.py
@@ -25,12 +25,12 @@ vzorec_razvijalcev = re.compile(r'<h5 b-12vzocx8j4>Developers</h5>.*?(?P<razvija
                                 flags=re.DOTALL)
 vzorec_razvijalca = re.compile(r'<a href=".*?/(?P<kljuc_razvijalca>\d+?)-.*?">(?P<razvijalec>.*?)</a>',
                                               flags=re.DOTALL)
-vzorec_izdajateljev = re.compile(r'''<h5 b-12vzocx8j4>Publishers</h5>.*?(?P<izdajatelji>(?:<a href=".*?">.*?</a>\s*,?\s*|<p b-12vzocx8j4 class="no-info">No information available</p>\s*)+).*?''',
+vzorec_zaloznikov = re.compile(r'''<h5 b-12vzocx8j4>Publishers</h5>.*?(?P<zalozniki>(?:<a href=".*?">.*?</a>\s*,?\s*|<p b-12vzocx8j4 class="no-info">No information available</p>\s*)+).*?''',
                                  flags=re.DOTALL)
-vzorec_izdajatelja = re.compile(r'<a href=".*?/(?P<kljuc_izdajatelja>\d+?)-.*?">(?P<izdajatelj>.*?)</a>',
+vzorec_zaloznika = re.compile(r'<a href=".*?/(?P<kljuc_zaloznika>\d+?)-.*?">(?P<zaloznik>.*?)</a>',
                                               flags=re.DOTALL)
 
-#print(re.findall(vzorec_izdajateljev, prebrano))
+#print(re.findall(vzorec_zaloznikov, prebrano))
 
 
 def pocisti_znake(besedilo):
@@ -58,7 +58,7 @@ def podatki_igre(vsebina):
     igra['maks_st_igralcev'] = int(igra['maks_st_igralcev']) if igra['maks_st_igralcev'] != 'No information available' else None
     igra['daljsi_opis'] = pocisti_znake(html.unescape((igra['daljsi_opis'].strip())))
     
-    #ker se pri izdajateljih in razvijalcih nekateri ključi ponavljajo bom razvijalcem dodala začetne stevke 666, izdajateljem pa 555
+    #ker se pri zaloznikih in razvijalcih nekateri ključi ponavljajo bom razvijalcem dodala začetne stevke 666, zaloznikom pa 555
 
     razvijalci = str(vzorec_razvijalcev.findall(vsebina))[2:-1].split(',')
     igra['razvijalci'] = []
@@ -68,15 +68,15 @@ def podatki_igre(vsebina):
         razvijalec['kljuc_razvijalca'] = int('666' + razvijalec['kljuc_razvijalca'])
         igra['razvijalci'].append(razvijalec)
     
-    izdajatelji = str(vzorec_izdajateljev.findall(vsebina))[2:-1].split(',')
-    igra['izdajatelji'] = []
-    for el in izdajatelji:
-        ujemanje = vzorec_izdajatelja.search(el)         
+    zalozniki = str(vzorec_zaloznikov.findall(vsebina))[2:-1].split(',')
+    igra['zalozniki'] = []
+    for el in zalozniki:
+        ujemanje = vzorec_zaloznika.search(el)         
         if ujemanje:
-            izdajatelj = ujemanje.groupdict()  
-            izdajatelj['izdajatelj'] = html.unescape((izdajatelj['izdajatelj']))
-            izdajatelj['kljuc_izdajatelja'] = int('555' + izdajatelj['kljuc_izdajatelja'])
-            igra['izdajatelji'].append(izdajatelj)
+            zaloznik = ujemanje.groupdict()  
+            zaloznik['zaloznik'] = html.unescape((zaloznik['zaloznik']))
+            zaloznik['kljuc_zaloznika'] = int('555' + zaloznik['kljuc_zaloznika'])
+            igra['zalozniki'].append(zaloznik)
     
 
     igra = OrderedDict(
@@ -88,7 +88,7 @@ def podatki_igre(vsebina):
         maks_st_igralcev=igra["maks_st_igralcev"],
         zanri=igra["zanri"],
         razvijalci=igra["razvijalci"],
-        izdajatelji=igra["izdajatelji"],
+        zalozniki=igra["zalozniki"],
         daljsi_opis=igra["daljsi_opis"]
     )
     
--- funkcije/shrani_v_json_in_csv.py
+++ funkcije/shrani_v_json_in_csv.py
@@ -4,7 +4,7 @@ import csv
 from uredi_tabele import *
 
 zapisi_json(igre, 'json_in_csv_datoteke/glavno_igre.json')
-zapisi_csv(igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "zanri", "razvijalci", "izdajatelji", "daljsi_opis"], 'json_in_csv_datoteke/glavno_igre.csv')
+zapisi_csv(igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "zanri", "razvijalci", "zalozniki", "daljsi_opis"], 'json_in_csv_datoteke/glavno_igre.csv')
 
 
 zapisi_json(vse_igre, 'json_in_csv_datoteke/igre.json')
@@ -12,10 +12,10 @@ zapisi_csv(vse_igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_s
 
 zapisi_json(zanri, 'json_in_csv_datoteke/zanri_iger.json')
 zapisi_json(razvijalci, 'json_in_csv_datoteke/razvijalci_iger.json')
-zapisi_json(izdajatelji, 'json_in_csv_datoteke/izdajatelji_iger.json')
+zapisi_json(zalozniki, 'json_in_csv_datoteke/zalozniki_iger.json')
 
        
 zapisi_csv(zanri, ["kljuc_igre", "zanr"], 'json_in_csv_datoteke/zanri_iger.csv')
 zapisi_csv(razvijalci, ["kljuc_igre", "kljuc_razvijalca", "razvijalec"], 'json_in_csv_datoteke/razvijalci_iger.csv')
-zapisi_csv(izdajatelji, ["kljuc_igre","kljuc_izdajatelja", "izdajatelj"], 'json_in_csv_datoteke/izdajatelji_iger.csv')
+zapisi_csv(zalozniki, ["kljuc_igre","kljuc_zaloznika", "zaloznik"], 'json_in_csv_datoteke/zalozniki_iger.csv')
 
