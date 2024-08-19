c9498efee598962e4e7cf12753dfda52c7209375
masapopovic <mp13284@student.uni-lj.si>
2024-08-20 00:19:11 +0200

    popravek poimenovanj

--- funkcije/shrani_v_json_in_csv.py
+++ funkcije/shrani_v_json_in_csv.py
@@ -4,11 +4,11 @@ import csv
 from uredi_tabele import *
 
 zapisi_json(igre, 'json_in_csv_datoteke/glavno_igre.json')
-zapisi_csv(igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "zanri", "razvijalci", "zalozniki", "daljsi_opis"], 'json_in_csv_datoteke/glavno_igre.csv')
+zapisi_csv(igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "zanri", "razvijalci", "zalozniki", "daljsi_opis"], 'Analiza_ter_json_in_csv_datoteke/glavno_igre.csv')
 
 
 zapisi_json(vse_igre, 'json_in_csv_datoteke/igre.json')
-zapisi_csv(vse_igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "daljsi_opis"], 'json_in_csv_datoteke/igre.csv')
+zapisi_csv(vse_igre, ["ime", "kljuc_igre", "ocena", "leto", "platforma", "maks_st_igralcev", "daljsi_opis"], 'Analiza_ter_json_in_csv_datoteke/igre.csv')
 
 
 zapisi_json(zanri, 'json_in_csv_datoteke/zanri_iger.json')
@@ -16,7 +16,7 @@ zapisi_json(razvijalci, 'json_in_csv_datoteke/razvijalci_iger.json')
 zapisi_json(zalozniki, 'json_in_csv_datoteke/zalozniki_iger.json')
 
  
-zapisi_csv(zanri, ["kljuc_igre", "zanr"], 'json_in_csv_datoteke/zanri_iger.csv')
-zapisi_csv(razvijalci, ["kljuc_igre", "kljuc_razvijalca", "razvijalec"], 'json_in_csv_datoteke/razvijalci_iger.csv')
-zapisi_csv(zalozniki, ["kljuc_igre","kljuc_zaloznika", "zaloznik"], 'json_in_csv_datoteke/zalozniki_iger.csv')
+zapisi_csv(zanri, ["kljuc_igre", "zanr"], 'Analiza_ter_json_in_csv_datoteke/zanri_iger.csv')
+zapisi_csv(razvijalci, ["kljuc_igre", "kljuc_razvijalca", "razvijalec"], 'Analiza_ter_json_in_csv_datoteke/razvijalci_iger.csv')
+zapisi_csv(zalozniki, ["kljuc_igre","kljuc_zaloznika", "zaloznik"], 'Analiza_ter_json_in_csv_datoteke/zalozniki_iger.csv')
 
